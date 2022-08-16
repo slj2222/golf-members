9843fc7dcce41878eeae4e3a9b3da3adaf07fc23
Spencer Johnson <sljohnson2222@gmail.com>
2022-08-15 23:37:48 -0500

    api connected

--- golf-club-react/src/App.js
+++ golf-club-react/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/members")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage />
     </div>
   );
 }
