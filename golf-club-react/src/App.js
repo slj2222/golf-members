bb8efbd16be5ed14eb42516449eb1591377d42f3
Spencer Johnson <sljohnson2222@gmail.com>
2022-08-15 23:45:59 -0500

    member react pulling through on MemberCard

--- golf-club-react/src/App.js
+++ golf-club-react/src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [allMembers, setAllMembers] = useState([])
+
   useEffect(() => {
     fetch("http://localhost:3000/api/v1/members")
-        .then(res => res.json())
-        .then(data => console.log(data))
-}, [])
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setAllMembers(data)
+      })
+  }, [])
+
+  
 
   return (
     <div className="App">
-      <LandingPage />
+      <LandingPage allMembers={allMembers} />
     </div>
   );
 }
--- golf-club-react/src/components/LandingPage.js
+++ golf-club-react/src/components/LandingPage.js
@@ -1,15 +1,20 @@
+import MemberCard from "./MemberCard"
 
-export default function LandingPage() {
+export default function LandingPage( {allMembers} ) {
 
 
 
-
+    const mapAllMembers = allMembers.map(member => {
+        return(
+            <MemberCard member={member}/>
+        )
+    })
 
 
 
     return (
         <div>
-            Working? 
+            {mapAllMembers}
         </div>
     )
 }
