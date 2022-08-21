6b9319ff7f97e4ba18acfd2608af975446f21ba5
Spencer Johnson <sljohnson2222@gmail.com>
2022-08-21 01:17:15 -0500

    day and time set for 14 days

--- golf-club-react/src/components/LandingPage.js
+++ golf-club-react/src/components/LandingPage.js
@@ -14,16 +14,37 @@ console.log(availableTeeTimeTimeArray)
     useEffect(() => {
         
         let teeTimeTimeArrayUnix = []
-        // console.log(teeTimeTimeArray)
+        console.log(teeTimeTimeArrayUnix)
 
-        //get todays date and set time to 6:47am to offset the start time of 7:00am in for loop
-        const todaysDate = new Date().setHours(6,47,0)
-        // console.log(todaysDate)
-        //converts back to date/time object with another new Date()
-        const resetTime = (new Date(todaysDate))
-        // console.log(resetTime)
+
+        function indexDay() {
+            //get todays date and set time to 6:47am to offset the start time of 7:00am in for loop
+            const changeHours = new Date().setHours(6,47,0)
+            //new date
+            const withHoursChanged = new Date(changeHours)
+            //gets previously changed time and changes the date back one day
+            const changeDate = withHoursChanged.setDate(withHoursChanged.getDate() - 1)
+
+            //converts back to date/time object with another new Date()
+            const resetTime = new Date(changeDate)
+            // console.log(resetTime)
+            // addMinutes(resetTime)
+            addDays(resetTime)
+        }
+
+        function addDays(teeTimeDays) {
+            const n = 14
+            for (let i = 0; i < n; i++) {
+                let newTeeTimeDay = teeTimeDays.setDate(teeTimeDays.getDate() + 1)
+                let newDayVersion = new Date(newTeeTimeDay)
+                // console.log(newTeeTimeDay)
+                addMinutes(newDayVersion)
+            }
+        }
+
         function addMinutes(teeTimeHours) {
-            // console.log(typeof(todaysDate))
+            // console.log(typeof(teeTimeHours))
+            // console.log(teeTimeHours)
             //set the number of times it needs to add 13 min
             const n = 43
                 for (let i = 0; i < n; i++) {
@@ -32,17 +53,19 @@ console.log(availableTeeTimeTimeArray)
                     let newTimeVersion = new Date(newTeeTimeHours)
                     teeTimeTimeArrayUnix.push(newTimeVersion)    
                 }
-                // console.log(teeTimeTimeArray)
         }
-        addMinutes(resetTime)
         
-
+        indexDay()
         setAvailableTeeTimeTimeArray(teeTimeTimeArrayUnix)
-        
         
     }, [])
 
-    // console.log(availableTeeTimeTimeArray)
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++
     
     const mapAvailableTeeTimeTimeArray = availableTeeTimeTimeArray.map(time => {
         return (
@@ -50,25 +73,6 @@ console.log(availableTeeTimeTimeArray)
             <TimeCard key={time} date={time.toLocaleDateString()} time={time.toLocaleTimeString()} />
         )
     })
-
-
-    // function addDays(date = new Date()) {
-    //     date.setDate(date.getDate() + 1)
-    //     // console.log(date)
-    // }
-    // addDays()
-  
-
-
-
-    // const n = 10
-    // for (let i = 0; i < n; i++) { 
-    //  addMinutes()
-    // }
-        
-    
-
-
 
     return (
         <div>
