96974e6d4a444487163b7abddfe803cb55307fd5
Spencer Johnson <sljohnson2222@gmail.com>
2022-09-03 02:10:34 -0500

    dependency added for useEffect(), persists on refresh now

--- golf-club-react/src/components/DayView.js
+++ golf-club-react/src/components/DayView.js
@@ -4,21 +4,12 @@ import { useParams } from "react-router-dom";
 
 export default function DayView({ apiReservations, removeTeeTime }) {
     // console.log(selectedDay)
-    // const selectedDayConverted = new Date(selectedDay)
-    // console.log(selectedDayConverted)
-    const [availableTeeTimeTimeArray, setAvailableTeeTimeTimeArray] = useState([])
-    // console.log(availableTeeTimeTimeArray)
+
     const [teeTimeTimeArrayUnix, setTeeTimeTimeArrayUnix] = useState([])
-    // console.log(teeTimeTimeArrayUnix)
-    // const [apiReservationsReservationTime, setApiReservationsReservationTime] = useState([])
-    // const [actualAvailableTeeTimeArray, setActualAvailableTeeTimeArray] = useState([])
-    // console.log(apiReservationsReservationTime)
+
     const { id } = useParams()
     
     const selectedDayConverted = new Date(parseInt(id))
-    // console.log(id)
-    // console.log(selectedDayConverted)
-    // console.log(teeTimeTimeArrayUnix)
 
     const apiReservationTimes = apiReservations.map(reservation => reservation.reservation_time) 
     // console.log(typeof(apiReservationTimes))
@@ -48,51 +39,10 @@ export default function DayView({ apiReservations, removeTeeTime }) {
 
 // +++++++++++++++++++++
 
-
-                // const apiArr = []
-        // console.log(apiArr)
-        // console.log(apiReservations[1])
-        // function mapApiReservations(apiReservations) {
-            // const tempArr2 = []
-            // console.log(tempArr2)
-            
-            // setApiReservationsReservationTime(apiReservationTimes)
-            // const apiMap = apiReservations.map(reservation => reservation.reservation_time)
-
-            // const spreadNewArr = [...apiArr, apiMap]
-            // console.log(spreadNewArr)
-        // }   
-        // mapApiReservations(apiReservations)
-
-        // function compareApiReservationsAndTeeTimeTimeArrayUnix(reservationTime){
-        //     console.log(reservationTime)
-        //     teeTimeTimeArrayUnix.filter(teeTime => reservationteeTime !== reservationTime))
-        //     // console.log(filteredTeeTimes)
-        //     // setAvailableTeeTimeTimeArray(filteredTeeTimes)
-        // }
-        // compareApiReservationsAndTeeTimeTimeArrayUnix(newArr)
-        // console.log(teeTimeTimeArrayUnix)
-        // console.log(newArr1)
-        
-
-                
-        
-        
-        
-
-
-
-// console.log(test)
-
 // +++++++++++++++++++++
         addMinutes(selectedDayConverted)
-        // setAvailableTeeTimeTimeArray(teeTimeTimeArrayUnix)
-        // setAvailableTeeTimeTimeArray(end)
-        // const end = teeTimeTimeArrayUnix.filter(time => time !== 1663016760417)
-        const end = teeTimeTimeArrayUnix.filter(time => apiReservationTimes.includes(time))
-        setAvailableTeeTimeTimeArray(end)
-        console.log(end)
-    }, [])
+        
+    }, [apiReservations])
 
     // 1662498360262
 
